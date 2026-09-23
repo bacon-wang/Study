2fd6ae4307d62483462525b20868f147c2b2ee94
beibei <mac@beibeiMacBook-Pro.local>
2026-09-23 23:41:06 +0800

    初识 Java 演示

---
+++ JavaSE/1-intro/src/HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ * 这是文档注释
+ */
+
+// 这是行注释
+
+/*
+    这是块注释
+ */
+
+public class HelloWorld {
+    /**
+     * Main 方法
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+//        System.out.println("你好！");
+
+        /*
+        System.out.println("你好！");
+        System.out.println("你好！");
+         */
+    }
+}
+
+class Test {
+
+}
